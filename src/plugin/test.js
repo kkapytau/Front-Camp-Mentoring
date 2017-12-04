69aecf9ff8869b7bcfadd2ba15b95d317fe273c8
Kanstantsin Kapytau <kanstantsin_kapytau@epam.com>
2017-12-04 16:34:01 +0300

    Add custom plugin (remove console.log statement)

---
+++ src/plugin/test.js
@@ -0,0 +1,19 @@
+console.log("ZZZ");
+function square(n) {
+  console.log(n);
+  return n * n;
+  console.log(n);
+}
+n;
+console.log("AAA");
+
+class test{
+  constructor(){
+    console.log("in")
+  }
+  testMethod(){
+    console.log("testMethod")
+  }
+}
+
+console.log("Finished");
