eda2bf7f9a06b750415c7cbab160ac9ab91474a1
Kanstantsin Kapytau <kanstantsin_kapytau@epam.com>
2017-12-10 17:54:47 +0300

    Update prod config

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: {
     news: [
-      'promise-polyfill',
+      'babel-polyfill',
       './src/app/App.js'
     ]
   },
@@ -39,6 +39,11 @@ module.exports = {
           fallback: 'style-loader',
           use: ['css-loader', 'less-loader']
         })
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
       },
       {
         test: /\.css$/,
