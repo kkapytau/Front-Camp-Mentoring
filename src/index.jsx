9c1e3591ba162bcb0db298fa6b3d3d6438c0a77c
Kanstantsin <kanstantsin_kapytau@epam.com>
2018-02-21 14:49:30 +0300

    add files

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Wrapper from './components/app/wrapper';
+
+ReactDOM.render(
+  <Wrapper />,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  context: path.join(__dirname, 'src'),
+  entry: {
+    blogs: [
+      'react-hot-loader/patch',
+      './index.jsx'
+    ]
+  },
+  output: {
+    path: path.join(__dirname, './build'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    port: 8081
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader']
+        })
+
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader?url=false&minimize=true', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Test',
+      hash: true,
+      template: './index.html'
+    }),
+    new ExtractTextPlugin({
+      filename: 'main.css',
+      allChunks: true
+    })
+  ]
+};
+
+module.exports = config;
